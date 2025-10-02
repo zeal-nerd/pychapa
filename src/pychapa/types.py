9512a852cdf336e7cbd561fa2e1cb3373935861c
zix <zix@fedora>
2025-10-02 21:45:57 +0300

    separate module for types

---
+++ src/pychapa/types.py
@@ -0,0 +1,8 @@
+from typing import Literal
+
+# --------------- Types
+ModeLiteral = Literal["test", "live"]
+StatusLiteral = Literal["pending", "success"]
+CurrencyLiteral = Literal["ETB", "USD"]
+HttpMethodLiteral = Literal["get", "post"]
+SplitTypeLiteral = Literal["flat", "percentage"]
